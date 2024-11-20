025d577b70bf22b100e67f2f67365086c826c524
CH4N <84262895+MangOleh22@users.noreply.github.com>
2024-11-20 08:30:03 +0700

    Update anggota.php

--- admin/anggota.php
+++ admin/anggota.php
@@ -11,7 +11,7 @@ if (empty($_SESSION['username'])){
     <meta charset="UTF-8">
     <title>ILibrary (Library Management System)</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <meta name="description" content="Hakko Bio Richard">
+    <meta name="description" content="Kelompok 4">
     <meta name="keywords" content="Perpus, Website, Aplikasi, Perpustakaan, Online">
     <!-- bootstrap 3.0.2 -->
     <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
@@ -54,7 +54,7 @@ if (empty($_SESSION['username'])){
         <!-- header logo: style can be found in header.less -->
         <header class="header">
             <a href="index.php" class="logo">
-                PerPusWeb
+                ILibrary
             </a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top" role="navigation">
@@ -228,7 +228,7 @@ $_SESSION['start_time'] = time();
               <!-- row end -->
                 </section><!-- /.content -->
                 <div class="footer-main">
-                    Copyright &copy <a href="http://www.hakkoblogs.com" target="_blank">I-Library</a> 2024
+                    Copyright &copy <a href="http://www.hakkoblogs.com" target="_blank">ILibrary</a> 2024
                 </div>
             </aside><!-- /.right-side -->
 
